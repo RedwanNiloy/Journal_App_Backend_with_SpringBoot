5685dd2577a045164b2d0298ad965a83393ef54a
RedwanNiloy <redwanniloy161293@gmail.com>
2024-07-02 02:23:12 +0600

    health-check-api completed

--- src/main/java/com/java/dev/journalApp/controller/JournalEntryController.java
+++ src/main/java/com/java/dev/journalApp/controller/JournalEntryController.java
@@ -2,7 +2,7 @@ package com.java.dev.journalApp.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController //special type of component who handles http method
 public class JournalEntryController{
 
     
