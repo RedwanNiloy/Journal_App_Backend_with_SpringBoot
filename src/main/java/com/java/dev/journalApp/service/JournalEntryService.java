9d2bf3d7925e5baba82cd039b0e68c00e8768ab0
RedwanNiloy <redwanniloy161293@gmail.com>
2024-09-01 02:06:02 +0600

    Transaction set

--- src/main/java/com/java/dev/journalApp/service/JournalEntryService.java
+++ src/main/java/com/java/dev/journalApp/service/JournalEntryService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.java.dev.journalApp.entity.JournalEntry;
 import com.java.dev.journalApp.entity.UserEntry;
@@ -28,7 +29,7 @@ public class JournalEntryService{
 
  
 
-  
+@Transactional
  public void saveEntry(JournalEntry entry, String email)
 {
      JournalEntry saved =journalreop.save(entry);
